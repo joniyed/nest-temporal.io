dd100d14e33e3d30e7ef35cb7a01cad6c243e4e1
joniyed.bhuiyan <joniyed.bhuiyan@gmail.com>
2025-01-29 16:49:08 +0600

    temporal config issue fixed

--- src/temporal/temporal.module.ts
+++ src/temporal/temporal.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TemporalService } from './temporal.service';
 import { Activities } from './activities';
-import { EmailService } from '../email/email.service';
 import { EmailModule } from '../email/email.module';
 
 @Module({
